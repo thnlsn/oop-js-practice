8a1ffda5b2a47db72342187198cd652f35ddeceb
Thomas <thnlsn.dev@gmail.com>
2021-05-05 21:48:05 -0700

    created Person constructor function

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 "use strict";
+
+// Arrow functions do not work for constructors because they do not have 'this' which is required
+const Person = function (firstName, birthyear) {};
+
+// The difference between a normal function and a constructor function is that a constructor function is called with new keyword (i.e. new String())
