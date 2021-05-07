4d7b0b568b31c3c7122ea248fc002434306135dc
Thomas <thnlsn.dev@gmail.com>
2021-05-06 22:29:28 -0700

    setting constructor state

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 
 // Arrow functions do not work for constructors because they do not have 'this' which is required
 const Person = function (firstName, birthyear) {
-  console.log(this);
+  this.firstName = firstName;
+  this.birthyear = birthyear;
 };
 
 // The difference between a normal function and a constructor function is that a constructor function is called with new keyword (i.e. new String())
 // new is an operator because it ___
-new Person("Thomas", 1997);
+const thomas = new Person("Thomas", 1997);
+console.log(thomas);
 
 // Calling a function with the new operator does a lot more than just invoking the function...
 // 1. New {} is created
 // 2. Function is called, and 'this' = {}
 // 3. {} is linked to prototype
-// 4. function automatically returns the {}
+// 4. Function automatically returns the {}, so it is the value of the {} when called.
