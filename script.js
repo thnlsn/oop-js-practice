9d21ddbe52ddc61b88af1cbfa852f996f06bab94
Thomas <thnlsn.dev@gmail.com>
2021-05-06 22:26:42 -0700

    comments explaining a new Function call under the hood

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 "use strict";
 
 // Arrow functions do not work for constructors because they do not have 'this' which is required
-const Person = function (firstName, birthyear) {};
+const Person = function (firstName, birthyear) {
+  console.log(this);
+};
 
 // The difference between a normal function and a constructor function is that a constructor function is called with new keyword (i.e. new String())
+// new is an operator because it ___
 new Person("Thomas", 1997);
+
+// Calling a function with the new operator does a lot more than just invoking the function...
+// 1. New {} is created
+// 2. Function is called, and 'this' = {}
+// 3. {} is linked to prototype
+// 4. function automatically returns the {}
