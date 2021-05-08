b65cf778b5c2582572d560022b84d157b3e9f211
Thomas <thnlsn.dev@gmail.com>
2021-05-07 22:52:55 -0700

    new Person from blueprint

--- script.js
+++ script.js
@@ -10,11 +10,14 @@ const Person = function (firstName, birthyear) {
 // new is an operator because it ___
 const thomas = new Person("Thomas", 1997);
 console.log(thomas);
-const marilyn = new Person("Marilyn", 1997);
-console.log(marilyn);
 
 // Calling a function with the new operator does a lot more than just invoking the function...
 // 1. New {} is created
 // 2. Function is called, and 'this' = {}
 // 3. {} is linked to prototype
 // 4. Function automatically returns the {}, so it is the value of the {} when called.
+
+const marilyn = new Person("Marilyn", 1997);
+const ethan = new Person("Ethan", 1997);
+console.log(marilyn);
+console.log(ethan);
